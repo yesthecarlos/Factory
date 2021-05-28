96a6a99e841b4086bba7b5f0b7367715584450b6
yesthecarlos <carlosmendez86@gmail.com>
2021-05-28 13:57:33 -0700

    Add Migrations folder and fix MachineId column in MachineEngineer.cs

--- Models/MachineEngineer.cs
+++ Models/MachineEngineer.cs
@@ -4,7 +4,7 @@ namespace FactorySpace.Models
     {       
         public int MachineEngineerId { get; set; }
         public int EngineerId { get; set; }
-        public int MachinedId { get; set; }
+        public int MachineId { get; set; }
         public virtual Engineer Engineer { get; set; }
         public virtual Machine Machine { get; set; }
     }
