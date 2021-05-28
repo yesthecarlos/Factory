7d3113c0fef1eaaa64eaccad4cc4244528b0cb55
yesthecarlos <carlosmendez86@gmail.com>
2021-05-28 16:11:13 -0700

    Add MachinesController.cs

---
+++ Controllers/MachinesController.cs
@@ -0,0 +1,71 @@
+using Microsoft.AspNetCore.Mvc;
+using FactorySpace.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace FactorySpace.Controllers
+{
+  public class MachinesController : Controller
+  {
+    private readonly FactorySpaceContext _db;
+
+    public MachinesController(FactorySpaceContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      List<Machine> model = _db.Machines.ToList();
+      return View(model);
+    }
+    public ActionResult Edit(int id)
+    {
+      var thisMachine = _db.Machines.FirstOrDefault(machine => machine.MachineId == id);
+      return View(thisMachine);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Machine machine)
+    {
+      _db.Entry(machine).State = EntityState.Modified;
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+    public ActionResult Create()
+    {
+      return View();
+    }
+    public ActionResult Delete(int id)
+    {
+      var thisMachine = _db.Machines.FirstOrDefault(machine => machine.MachineId == id);
+      return View(thisMachine);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      var thisMachine = _db.Machines.FirstOrDefault(machine => machine.MachineId == id);
+      _db.Machines.Remove(thisMachine);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    [HttpPost]
+    public ActionResult Create(Machine machine)
+    {
+      _db.Machines.Add(machine);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+    public ActionResult Details(int id)
+    {
+        var thisMachine = _db.Machines
+        .Include(machine => machine.JoinEntities)
+        .ThenInclude(join => join.Engineer
+        .FirstOrDefault(machine => machine.MachineId == id);
+    return View(thisMachine);
+    }
+  }
+}
